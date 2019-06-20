6fe8ea726d9570e0396e7fc92724dbb299f08529
Roger Pettett <rmp@psyphi.net>
2019-06-20 16:30:13 +0100

    test epi2me casks

--- Casks/epi2me-cli.rb
+++ Casks/epi2me-cli.rb
@@ -1,9 +1,9 @@
-class Epi2meCli < Formula
-  desc "EPI2ME CLI (stable)"
-  homepage "https://metrichor.com/"
-  url "https://cdn.oxfordnanoportal.com/software/metrichor-agent/epi2me-cli-macos-2.61.2367682"
+cask "epi2me-cli" do
+  version "2.61.2367682"
   sha256 "962ecbd567ffedc75534ace6dd19b22e8b75c32348c3156b7f9e801431a72f0e"
-  test do
-    system "epi2me-cli-2.61.2367682", "--selftest"
-  end
+
+  url "https://cdn.oxfordnanoportal.com/software/metrichor-agent/epi2me-cli-macos-#{version}"
+  name "EPI2ME CLI"
+
+  homepage "https://metrichor.com/"
 end
